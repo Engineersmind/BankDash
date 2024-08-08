ebb3ca46d7ab15cc30c82c77b448719cb4994af6
Mahammad Kutubbhai Chharchhodawala <mchharchhodawala@aipl6862lt15826.local>
2024-08-08 16:51:05 +0530

    layout changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { Settings } from "./pages/Settings";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <aside className="app-sidebar">sidebar</aside>
+
+      <header className="app-header">header</header>
+
+      <main className="app-main">
+        <Settings />
+      </main>
+    </>
   );
 }
 
---
+++ src/pages/Settings.js
@@ -0,0 +1,47 @@
+export function Settings() {
+  return (
+    <div className="app-card">
+      <div className="settings">
+        <div className="image-column">
+          <img />
+        </div>
+        <div>
+          <div className="row">
+            <div className="column">
+              <div className="form-group">
+                <label>Your Name</label>
+                <input type="text" className="form-input" />
+              </div>
+            </div>
+            <div className="column">
+              <div className="form-group">
+                <label>Your Name</label>
+                <input type="text" className="form-input" />
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="column">
+              <div className="form-group">
+                <label>Your Name</label>
+                <input type="text" className="form-input" />
+              </div>
+            </div>
+            <div className="column">
+              <div className="form-group">
+                <label>Your Name</label>
+                <input type="text" className="form-input" />
+              </div>
+            </div>
+          </div>
+          
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
